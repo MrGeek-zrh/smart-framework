f1bfc8082ad9d82a35b9b380aeff430c06977c86
mrgeek-zrh <2262356245@qq.com>
2020-09-28 17:26:04 +0800

    2020-09-28:项目代码注解补充，以及代码优化

--- src/main/java/com/smartframework/controller/DispatcherServlet.java
+++ src/main/java/com/smartframework/controller/DispatcherServlet.java
@@ -59,47 +59,75 @@ public class DispatcherServlet extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		//获取请求方法与请求路径
 		String requestMethod = request.getMethod().toLowerCase();
 		String requestPath = request.getPathInfo();
-		//获取Action处理器
+		
+		//调用ControllerHelper方法，通过Request对象获取对应的Handler处理器
 		Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
+		//判断有没有成功获取handler处理器
 		if (handler!=null) {
+			//成功获取hanlder处理器
 			//获取Controller类及其Bean实例
 			Class<?>controllerClass = handler.getControllerClass();
 			Object controllerBean = BeanHelper.getBean(controllerClass);
-			//创建请求参数对象
+			
+			//创建请求参数对象，用于保存request中所有的参数的key和value
 			Map<String, Object>paramMap = new HashedMap();
+			
+			//获取request中所有的参数的名称
 			Enumeration<String>paramNames = request.getParameterNames();
+			//遍历集合，将所有的参数封装到Map集合中
 			while (paramNames.hasMoreElements()) {
+				//获取参数的名称
 				String paramName = (String) paramNames.nextElement();
+				//通过参数名称获取对应的值
 				String paramValue = request.getParameter(paramName);
+				//将参数的name和value封装到Map集合中
 				paramMap.put(paramName, paramValue);
 			}
+			
+			//将request中的参数进行解码，将参数还原
+			//body代表的是url中的参数部分，比如：https://r1---sn-ci5gup-cags.googlevideo.com/videoplayback?pcm2cms=yes&mime=video%2Fmp4
+										// 的？后面部分
 			String body = CodeUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
 			if (StringUtils.isNotEmpty(body)) {
+				//将字符串初步分割
 				String[] params = StringUtils.split(body, "&");
 				if (ArrayUtils.isNotEmpty(params)) {
 					for (String param : params) {
+						//将字符串进一步分割
 						String[] array = StringUtils.split(param, "=");
 						if (ArrayUtils.isNotEmpty(array)&&array.length==2) {
 							String paramName = array[0];
 							String paramValue = array[1];
+							//将处理好的参数封装进Map集合中
 							paramMap.put(paramName, paramValue);
 						}
 					}
 				}
 			}
+			
+			//将paramMap封装到Param实体类中
 			Param param = new Param(paramMap);
+			
 			//调用Action方法
 			Method actionMethod = handler.getActionMethod();
 			Object result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
 			
 			//处理Action方法返回值
+			//判断返回值的类型
+			//判断返回结果是否是JSP页面
 			if (result instanceof View) {
-				//返回JSP页面
+				//是View的实例，则返回JSP页面
+				
+				//将result强转为view类
 				View view = (View) result;
+				
+				//获取需要返回的jsp页面的具体路径
 				String path = view.getPath();
+				
 				if (StringUtils.isNotEmpty(path)) {
 					if (path.startsWith("/")) {
 						response.sendRedirect(request.getContextPath()+path);
@@ -111,6 +139,7 @@ public class DispatcherServlet extends HttpServlet{
 						request.getRequestDispatcher(ConfigHelper.getAPPJspBasePath()+path).forward(request, response);
 					}
 				}
+				//返回结果是JSON数据
 			}else if (result instanceof Data) {
 				//返回JSON数据
 				Data data = (Data) result;
@@ -118,8 +147,10 @@ public class DispatcherServlet extends HttpServlet{
 				if (model!=null) {
 					response.setContentType("application/json");
 					response.setCharacterEncoding("UTF-8");
+					//将数据转换为JSON字符串
 					PrintWriter writer = response.getWriter();
 					String json = JsonUtil.toJson(model);
+					//将字符床写入流中
 					writer.write(json);
 					writer.flush();
 					writer.close();
